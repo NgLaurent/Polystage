be5a297211a4415f28acd383454c5dfef4828968
NgLaurent <44085652+NgLaurent@users.noreply.github.com>
2020-01-16 09:06:56 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const config = {
- app: {
-   port: 8080
- },
- db: {
-   host: 'dbs-perso.luminy.univmed.fr',
-   port: 3306,
-   database: 'd17023188',
-   user: 'd17023188',
-   password: 'Ev-CZs7X'
- },
- desiree: {
-  id: 'd17023188',
-  mdp: 'Enilapo1996'
- }
+  app: {
+    port: 8080
+  },
+  db: {
+    host: 'dbs-perso.luminy.univmed.fr',
+    port: 3306,
+    database: 'd17023188',
+    user: 'd17023188',
+    password: 'Ev-CZs7X'
+  },
+  gmail: {
+    user: 'polystage.alert@gmail.com',
+    mdp: 'bilodu13'
+  }
 };
 
 
--- index.js
+++ index.js
@@ -9,11 +9,16 @@ const mysql = require('mysql');
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-const db = mysql.createConnection ({
+const db = mysql.createConnection({
     host: config.db.host,
     user: config.db.user,
-	  port: config.db.port,
+    port: config.db.port,
     password: config.db.password,
     database: config.db.database
 });
@@ -26,7 +31,9 @@ db.connect((err) => {
     }
     console.log('Connected to database');
 });
+
 global.db = db;
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html")
@@ -36,5 +43,5 @@ var routes = require('./api/routes/route'); //importing route
 routes(app); //register the route
 
 app.listen(port, function () {
-  console.log('Serveur up on http://localhost:3000/')
+    console.log('Serveur up on http://localhost:' + port)
 })
